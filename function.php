a4acee75948ed7291f817e41a92aded31bbda00a
Giulia Bini <ccampani@omniagroup.it>
2021-02-04 11:12:38 +0100

    Rimosso commenti e funzioni

--- function.php
+++ function.php
@@ -1,9 +1,5 @@
 <?php
 
-/*Commento per GitHub*/
-
-/*Commento per GitHub fatto da Flavio*/
-
 /*addizione*/
 
 function addizione (n1,n2) {
@@ -16,19 +12,4 @@ function sottrazione (n1,n2) {
 	return n1-n2;
 }
 
-/*moltiplicazione*/
-
-function moltiplicazione (n1,n2) {
-	return n1*n2;
-}
-
-/*divisione*/
-
-function divisione (n1,n2) {
-	if(n2 !== 0) {
-	return n1/n2;
-} else { 
-return "errore";
-}
-}
 ?>
